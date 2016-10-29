15c7dcae8bb72712a91e3b5cca0ec624adbf043f
theskillwithin <austinppeterson@gmail.com>
2016-10-29 01:53:41 -0600

    updateFish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,7 +10,8 @@ class App extends React.Component {
 	constructor() {
 		super();
 
-		this.addFish = this.addFish.bind(this);
+    this.addFish = this.addFish.bind(this);
+		this.updateFish = this.updateFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
 
@@ -60,6 +61,12 @@ class App extends React.Component {
 		this.setState({ fishes })
 	}
 
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes })
+  }
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -93,7 +100,12 @@ class App extends React.Component {
           order={this.state.order}
           params={this.props.params}
         />
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+				<Inventory 
+          addFish={this.addFish} 
+          loadSamples={this.loadSamples} 
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+          />
 			</div>
 		)
 	}
